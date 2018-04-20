ec6310b60dde21fb1bdd23d8af1c72e13df67052
grzspiteri <grzspiteri@live.com>
2018-04-20 15:25:01 +0200

    Downloaded website template
    We downloaded the website template using bootstrap to make the process quicker.

---
+++ application/views/template/end.php
@@ -0,0 +1,66 @@
+    <naV id="menu">
+        <ul>
+            <hr>
+
+            <li>
+                <a href="#">Contact Us</a>
+            </li>
+
+            <li>
+                About Us
+            </li>
+
+            <li>
+                <a href="#">News & Events</a>
+            </li>
+
+            <li>
+                <a href="#">Regulations</a>
+            </li>
+
+            <li>
+                <a href="http://www.mcast.edu.mt/inst/2">Full-Time Courses</a>
+            </li>
+
+            <li>
+                <a href="https://shortcourses.mcast.edu.mt/Institutes/InstituteInformation.aspx?id=E59BA0D5613C23756D0AABE3E07863D5">Part-Time Courses</a>
+            </li>
+
+            <li>
+                Student Links
+            </li>
+
+            <li>
+                <a href="#">Cancelled Lectures & Notices</a>
+            </li>
+
+            <li>
+                <a href="http://www.mcast.edu.mt/183">Handbook</a>
+            </li>
+
+            <li>
+                <a href="#">Useful Documents</a>
+            </li>
+
+            <li>
+                <a href="http://www.mcast.edu.mt/searchOurCatalogue">Library</a>
+            </li>
+
+            <li>
+                <a href="https://www.schoology.com/">Schoology</a>
+            </li>
+
+            <li>
+                <a href="#">Timetable</a>
+            </li>
+
+            <li>
+                <a href="http://attendance.mcast.edu.mt/MyAttendance">Attendance</a>
+            </li>
+
+        </ul>
+    </naV>
+
+        </main>
+    </body>
+</html>
---
+++ application/views/template/start.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ICA Website</title>
+
+    
+  </head>
+  <body>
+
+    <naV id="menu">
+        <ul>
+            <li>
+                About Us
+            </li>
+
+            <li>
+                <a href="#">News & Events</a>
+            </li>
+
+            <li>
+                <a href="#">Regulations</a>
+            </li>
+
+            <li>
+                <a href="http://www.mcast.edu.mt/inst/2">Full-Time Courses</a>
+            </li>
+
+            <li>
+                <a href="https://shortcourses.mcast.edu.mt/Institutes/InstituteInformation.aspx?id=E59BA0D5613C23756D0AABE3E07863D5">Part-Time Courses</a>
+            </li>
+
+            <li>
+                Student Portfolio
+            </li>
+
+            <li>
+                <a href="#">Interactive Media</a>
+            </li>
+
+            <li>
+                <a href="#">Graphics Design</a>
+            </li>
+
+            <li>
+                <a href="#">Game Art</a>
+            </li>
+
+            <li>
+                <a href="#">Media</a>
+            </li>
+
+            <li>
+                <a href="#">Photography</a>
+            </li>
+
+            <li>
+                <a href="#">Product Design</a>
+            </li>
+
+            <li>
+                <a href="#">Spatial Design</a>
+            </li>
+
+            <br>
+
+            <li>
+                <a href="#">Add a Portfolio</a>
+            </li>
+
+            <li>
+                Student Links
+            </li>
+
+            <li>
+                <a href="#">Cancelled Lectures & Notices</a>
+            </li>
+
+            <li>
+                <a href="http://www.mcast.edu.mt/183">Handbook</a>
+            </li>
+
+            <li>
+                <a href="#">Useful Documents</a>
+            </li>
+
+            <li>
+                <a href="http://www.mcast.edu.mt/searchOurCatalogue">Library</a>
+            </li>
+
+            <li>
+                <a href="https://www.schoology.com/">Schoology</a>
+            </li>
+
+            <li>
+                <a href="#">Timetable</a>
+            </li>
+
+            <li>
+                <a href="http://attendance.mcast.edu.mt/MyAttendance">Attendance</a>
+            </li>
+
+            <hr>
+
+            <li>
+                <a href="#">Contact Us</a>
+            </li>
+
+        </ul>
+    </naV>
+
+        <header id="title">
+            <h1>MCAST Institute for the Creative Arts</h1>
+        </header>
+
+        <main>
