73ea05449b840119a1ed4cc1a981367e76ce4267
grzspiteri <grzspiteri@live.com>
2018-05-04 16:13:33 +0200

    Login
    Added login navigation by using Css and html

--- application/views/template/start.php
+++ application/views/template/start.php
@@ -16,7 +16,9 @@
 
     <!-- Custom styles for this template -->
     <link href="css/modern-business.css" rel="stylesheet">
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
   </head>
 
   <body>
@@ -34,6 +36,9 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
+              <a class="nav-link" href="<?=site_url('home')?>">Home</a>
+            </li>
+            <li class="nav-item">
               <a class="nav-link" href="<?=site_url('about')?>">About Us</a>
             </li>
             <li class="nav-item">
@@ -45,8 +50,36 @@
             <li class="nav-item">
               <a class="nav-link" href="<?=site_url('contact')?>">Contact Us</a>
             </li>
-            <li class="nav-item" >
-              <a class="nav-link"  href="<?=site_url('login')?>">Login</a>
+            <li class="nav-item">
+              <div class="dropdown">
+
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Login
+                </a>
+
+                <form class="dropdown-menu p-4">
+                  <div class="form-group">
+                    <label for="exampleDropdownFormEmail2">Email address</label>
+                    <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleDropdownFormPassword2">Password</label>
+                    <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                    <label class="form-check-label" for="dropdownCheck2">
+                      Remember me
+                    </label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+
+
+          </div>
+      </div>
+              </div>
+
             </li>
 
 
